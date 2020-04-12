2e2f226a28da1775fc3a7e8ad716bad8e2d546f5
zhaox1n <zhao.x1n@foxmail.com>
2020-04-12 20:20:06 +0800

    Remove incorrect naming

--- parser-statement/src/main/java/org/github/zhaox1n/parser/segment/generic/database/DatabaseSegment.java
+++ parser-statement/src/main/java/org/github/zhaox1n/parser/segment/generic/database/DatabaseSegment.java
@@ -3,8 +3,7 @@ package org.github.zhaox1n.parser.segment.generic.database;
 import org.github.zhaox1n.parser.segment.SQLSegment;
 
 /**
- * @author : zhaoxin183
- * @date : 2020-04-10 17:30
+ * Database segment
  */
 public interface DatabaseSegment extends SQLSegment {
 }
